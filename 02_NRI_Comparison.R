8ce17819a2fbc76bf671d0740269395b46e184ef
Bing <145952666+This-Is-Bing@users.noreply.github.com>
2023-12-04 14:32:52 +0800

    Updating with NRI comparison

--- 02_NRI_Comparison.R
+++ 02_NRI_Comparison.R
@@ -3,8 +3,7 @@
 #----------------------------------------------------------------------------#
 # Please run the rproj file in the folder                                     
 # Dataset: Diabetes ACS, STEMI, NSTEMI on Testing Dataset                                    
-# Description: To compare the raw model & calibrated model performance 
-
+# Description: To compare the NRI Performance
 
 ##### Cleaning environment ####
 rm(list=ls())
@@ -82,19 +81,82 @@ stemi_calibrated_pred_probs <- predict(stemi_calibrated_model, data.frame(y = y_
 nstemi_raw_pred_probs <- predict(raw_model, X_nstemi_testing_ds, type= 'prob')$Death
 nstemi_calibrated_pred_probs <- predict(nstemi_calibrated_model, data.frame(y = y_nstemi_testing_ds, x = nstemi_raw_pred_probs), type = 'response')
 
+###### ACS—STEMI ####
+acs_stemi_raw_pred_probs <- predict(raw_model, X_stemi_testing_ds, type= 'prob')$Death
+
+###### ACS-NSTEMI ####
+acs_nstemi_raw_pred_probs <- predict(raw_model, X_nstemi_testing_ds, type= 'prob')$Death
 
 # ---------------------------------------------------------------------------#
-####                                 LIME                                 ####
+####                                  NRI                                 ####
 #----------------------------------------------------------------------------# 
+library(nricens)
 
-##### Reading Training dataset #####
-explainer <- lime(X_training_ds, raw_model)
-explanation <- explain(X_testing_ds[3, ], explainer, n_labels = 1, n_features = 10, n_permutations = 3, set.seed(10))
-plot_features(explanation)
+##### Search Threshold for Classification ####
+raw_threshold<-SearchBestThreshold(y_testing_ds,acs_raw_pred_probs)
+calibrated_threshold<-SearchBestThreshold(y_testing_ds,acs_calibrated_pred_probs)
 
-death_cases <- predict(raw_model, X_testing_ds, type= 'raw')
-death_index <- death_cases == 'Death'
-explanation <- explain(head(X_testing_ds[death_index,],4), explainer, n_labels = 1, n_features = 10, n_permutations = 3, set.seed(10))
-plot_features(explanation)
+stemi_raw_threshold<-SearchBestThreshold(y_stemi_testing_ds,stemi_raw_pred_probs)
+calibrated_stemi_raw_threshold<-SearchBestThreshold(y_stemi_testing_ds,stemi_calibrated_pred_probs)
+
+nstemi_raw_threshold<-SearchBestThreshold(y_nstemi_testing_ds,nstemi_raw_pred_probs)
+calibrated_nstemi_raw_threshold<-SearchBestThreshold(y_nstemi_testing_ds,nstemi_calibrated_pred_probs)
+
+stemi_acs_raw_threshold<-SearchBestThreshold(y_stemi_testing_ds,acs_stemi_raw_pred_probs)
+
+nstemi_acs_raw_threshold<-SearchBestThreshold(y_nstemi_testing_ds,nstemi_raw_pred_probs)
+
+
+##### Changing into Class 1,0 ####
+acs_raw_pred_probs_class <- ifelse(acs_raw_pred_probs>=raw_threshold, 1,0)
+acs_calibrated_pred_probs_class <- ifelse(acs_calibrated_pred_probs>=calibrated_threshold, 1,0)
+
+stemi_raw_pred_probs_class <- ifelse(stemi_raw_pred_probs>=stemi_raw_threshold, 1,0)
+stemi_calibrated_pred_probs_class <- ifelse(stemi_calibrated_pred_probs>=calibrated_stemi_raw_threshold, 1,0)
+
+nstemi_raw_pred_probs_class <- ifelse(nstemi_raw_pred_probs>=nstemi_raw_threshold, 1,0)
+nstemi_calibrated_pred_probs_class <- ifelse(nstemi_calibrated_pred_probs>=calibrated_nstemi_raw_threshold, 1,0)
+
+stemi_acs_raw_pred_probs_class <- ifelse(acs_stemi_raw_pred_probs>=stemi_acs_raw_threshold, 1,0)
+
+nstemi_acs_raw_pred_probs_class <- ifelse(acs_nstemi_raw_pred_probs>=nstemi_acs_raw_threshold, 1,0)
+
+##### Getting TIMI Score ####
+testing_ds <- read.csv('./data/processed/testing_ds.csv', header = TRUE)
+testing_ds$timiscorenstemi <- ifelse(testing_ds$timiscorenstemi >=5,1,0)
+testing_ds$timiscorestemi <- ifelse(testing_ds$timiscorestemi >=5,1,0)
+
+##### Separating  STEMI, NSTEMI ####
+stemi_testing_ds <- testing_ds[testing_ds$acsstratum ==1, ]
+nstemi_testing_ds <- testing_ds[!testing_ds$acsstratum ==1, ]
+
+##### Removing column ####
+stemi_testing_ds <- stemi_testing_ds[, -which(names(stemi_testing_ds) %in% c('acsstratum', 'timiscorenstemi'))]
+nstemi_testing_ds <- nstemi_testing_ds[, -which(names(nstemi_testing_ds) %in% c('acsstratum','timiscorestemi'))]
+
+##### TIMI Score ####
+testing_ds$timiscore <- ifelse(is.na(testing_ds$timiscorestemi), testing_ds$timiscorenstemi,testing_ds$timiscorestemi )
+timiscore <- testing_ds$timiscore
+
+acs_nri = nribin(event = y_testing_ds, 
+                 p.std=timiscore, 
+                 p.new=acs_calibrated_pred_probs_class, 
+                 cut = 0.5,
+                 niter = 5000, 
+                 updown = 'category')
+
+stemi_nri = nribin(event = stemi_testing_ds$ptoutcome, 
+                   p.std=stemi_testing_ds$timiscorestemi, 
+                   p.new=stemi_calibrated_pred_probs_class, 
+                   cut = 0.5,
+                   niter = 5000, 
+                   updown = 'category')
+
+nstemi_nri = nribin(event = nstemi_testing_ds$ptoutcome, 
+                    p.std=nstemi_testing_ds$timiscorenstemi, 
+                    p.new=nstemi_calibrated_pred_probs_class, 
+                    cut = 0.5,
+                    niter = 5000, 
+                    updown = 'category')
 
 
